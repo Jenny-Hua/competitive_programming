206bc47dacfd5381198fb73459b149261002265f
Jenny-Hua <jhuancaan@unsa.edu.pe>
2022-09-28 19:44:11 -0500

    Avance 12503 - Robot Instructions

---
+++ Set_01/12503.cpp
@@ -0,0 +1,52 @@
+// 12503 - Robot Instructions
+#include <cstdio>
+#include <string.h>
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int test;
+    int nInstr;             // nro. instrucciones
+    char instr[12];         // instruccion [longitud maxima]
+    int lineInstr;          // nro. de instr previa
+
+
+    scanf("%d", &test);
+    while(test)
+    {
+        int posicion = 0;
+        int I[nInstr];       // array I almacena n instr
+        int i;
+
+        scanf("%d", &nInstr);
+        // Almacenar en I[] las instrucciones de c/linea
+        for( i = 0; i < nInstr; i++ )
+        {
+            scanf("%s", instr);
+            if( !strcmp(instr, "LEFT"))         // strcmp return 0 if both are =
+                I[i] = -1;
+            else if( !strcmp(instr, "RIGHT"))
+                I[i] = 1;
+            else 
+            {
+                scanf("%s", instr);
+                scanf("%d", &lineInstr);
+                I[i] = I[lineInstr-1];
+            }
+        }
+        // Desarrollar c/instruccion partiendo del origen 0
+        for ( i = 0; i < nInstr; i++)
+        {
+            posicion = posicion + I[i];
+        }
+        printf("%d\n", posicion);
+
+        test --;
+    }
+    return 0;
+}
