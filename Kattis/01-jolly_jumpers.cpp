b4a7e7681336da3ef52b1f7495f08b0efe62aa4f
Jenny-Hua <jhuancaan@unsa.edu.pe>
2022-09-30 19:48:47 -0500

    jolly_jumpers

---
+++ Kattis/01-jolly_jumpers.cpp
@@ -0,0 +1,53 @@
+// Jolly Jumpers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+        freopen("in_01.txt", "r", stdin);
+        freopen("out_01.txt", "w", stdout);
+    #endif
+    
+    int cases;
+    while( cin>>cases)
+    {
+        int N[cases], R[cases-1], JJ[cases-1];
+        for (int i = 0; i < cases; i++)
+            cin>>N[i];
+
+        // Encontrar las diferencias en R e inicializar JJ en 0s
+        for (int j = 0; j < cases-1; j++)
+        {
+            R[j] = abs(N[j] - N[j + 1]);
+            JJ[j] = 0;
+        }
+
+        // Marcar como +1 si R[i] se encuentre en 1..cases-1
+        for (int j = 0; j < cases-1; j++){
+            for(int n = 1; n <= cases; n++){
+                if(n == R[j])
+                    JJ[n-1] +=1;
+            }
+        }
+        
+        bool isJJ = true;
+        // Recorrer JJ. Si la marca != 1, no es JJ
+        for (int l = 0; l < cases-1; l++)
+        {
+            // cout << JJ[l] << "\n";
+            if(JJ[l] != 1)
+                isJJ = false;
+        }
+
+        if(isJJ == true)
+            cout<<"Jolly\n";
+        else
+            cout<<"Not jolly\n";
+    }
+
+
+    return 0;
+}
