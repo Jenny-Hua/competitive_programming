e0778f957bb2cc6d4ddd665ce8eba9cf5694a780
Jenny-Hua <jhuancaan@unsa.edu.pe>
2022-10-08 08:13:47 -0500

    Almost Union Find solved!

--- Data Structures Contest/e_Almost_Union_Find.cpp
+++ Data Structures Contest/e_Almost_Union_Find.cpp
@@ -1,58 +1,75 @@
-// Problem E - Almost Union-Find
+// Problem E - Almost Union-findSet
 
 #include <iostream>
-
 using namespace std;
 
-int main()
+const int NMAX = 100001;        
+int padre[NMAX*2]={0};         // padre representativo
+int tam[NMAX*2]={0};           // nro de hijos
+long long sum[NMAX*2]={0};            // suma en padre representativo
+
+void makeSet ( int n ) {
+    for ( int i = 1, j = n + 1; i <= n; ++i, ++j ) {
+        padre[i] = j;  
+        padre[j] = j;
+        tam[j] = 1;
+        sum[j] = i;
+    }
+}
+
+int findSet ( int a ) {
+    if ( padre[a] == a ) return a;
+    return padre[a] = findSet(padre[a]);
+}
+
+void unionSets ( int a, int b ) {
+    a = findSet(a);
+    b = findSet(b);
+    if ( a != b ) {
+        tam[b] += tam[a];
+        sum[b] += sum[a];
+        padre[a] = b;
+    }
+}
+
+void moveToSet ( int a, int b ) {
+    int parentA = findSet(a);
+    int parentB = findSet(b);
+    if ( parentA != parentB ) {
+        tam[parentB] += 1;
+        tam[parentA] -= 1;
+        sum[parentB] += a;
+        sum[parentA] -= a;
+        padre[a] = parentB;
+    }
+}
+
+int main ( ) 
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     #ifndef ONLINE_JUDGE
         freopen("in_05.txt", "r", stdin);
         freopen("out_05.txt", "w", stdout);
     #endif
 
-    int n, o;
-    while(cin>>n>>o)
-    {
-        // Estructura de almacenamiento:
-        // Indice -> elemento                       i: 0 1 2 3
-        // S[indice]-> Almacena al representativo   r: 0 1 2 3
-        int S[n+1];
-        for(int i = 1; i <= n; i++)
-            S[i] = i;
-
-        // Operaciones
-        int op, p, q;
-        for (int j = 0; j < o; j++)
-        {
-            cin>>op;
-            switch (op)
-            {
-            case 1:                             // unir {p..} + {q..} -> {p .., q..}
-                cin>>p>>q;
-                for(int k = 1; k <= n; k++)     // k = elemento; S[k] = representativo
-                    if (S[k] == S[q])
-                        S[k] = S[p];
-                break;
-            
-            case 2:                             // mover p -> {q,..}
-                cin>>p>>q;
-                S[p] = S[q];
-                break;
-            case 3:                             // suma y nro de elemtos de p
-                cin>>p;
-                int counter = 0, suma = 0;
-                for(int k = 1; k <= n; k++)     // k = elemento; S[k] = representativo
-                {
-                    if(S[k] == S[p])
-                    {
-                        counter++;
-                        suma += k;
-                    }
-                }
-                cout << counter <<" "<< suma <<"\n";
-                break;
+    int N, M, Q, p, q;
+    while ( cin >> N >> M ) {
+        makeSet(N);
+        for ( int i = 0; i < M; ++i ) {
+            cin >> Q;
+            if ( Q == 1 ) {
+                cin >> p >> q;
+                unionSets(p, q);
+            } else if ( Q == 2 ) {
+                cin >> p >> q;
+                moveToSet(p, q);
+            } else {
+                cin >> p;
+                cout << tam[findSet(p)] << ' ' << sum[findSet(p)] << '\n';
             }
         }
     }
+    return 0;
 }
